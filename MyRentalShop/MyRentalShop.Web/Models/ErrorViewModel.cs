2d5f2233e010bfd8e71a86c391364221a0662bb9
PW <przemek.wrobel@poczta.fm>
2020-09-18 13:23:36 +0200

    Init

---
+++ MyRentalShop/MyRentalShop.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyRentalShop.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
