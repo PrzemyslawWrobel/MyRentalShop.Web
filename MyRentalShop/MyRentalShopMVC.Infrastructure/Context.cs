1a518e437806f4ce7e83c82f8c3efa89f8e96e0e
PW <przemek.wrobel@poczta.fm>
2020-09-30 23:10:12 +0200

    Dodanie context, usunięcie z web standardowego contextu i podmiana na utworzony w projekcie MyRentalShopMVC.Infrastructure

---
+++ MyRentalShop/MyRentalShopMVC.Infrastructure/Context.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using MyRentalShopMVC.Domain.Model;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Type = MyRentalShopMVC.Domain.Model.Type;
+
+namespace MyRentalShopMVC.Infrastructure
+{
+    public class Context : IdentityDbContext
+    {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<ContactDetail> ContactDetails { get; set; }
+        public DbSet<ContactDetailType> ContactDetailTypes { get; set; }
+        public DbSet<Customer> Customers { get; set; }
+        public DbSet<CustomerContactInformation> CustomerContactInformation { get; set; }
+        public DbSet<Item> Items { get; set; }
+        public DbSet<ItemTag> ItemTag { get; set; } // w liczbie poj ponieważ jest to tabela pośrednia w relacji wiele do wielu
+        public DbSet<Provider> Providers { get; set; }
+        public DbSet<Tag> Tags { get; set; }
+        public DbSet<Type> Types { get; set; }
+
+        public Context(DbContextOptions options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            ///Informacja po czym Entity ma zbudować relację 1 : 1
+
+            builder.Entity<Customer>()
+                .HasOne(a => a.CustomerContactInformation).WithOne(b => b.Customer)
+                .HasForeignKey<CustomerContactInformation>(e => e.CustomerRef);
+
+            ///Budowa relacji wiele do wielu przy pomocy tabeli pośredniczoncej
+
+            builder.Entity<ItemTag>()
+                .HasKey(it => new { it.ItemId, it.TagId });
+
+            builder.Entity<ItemTag>()
+                .HasOne<Item>(it => it.Item)
+                .WithMany(i => i.ItemTags)
+                .HasForeignKey(it => it.Item);
+
+            builder.Entity<ItemTag>()
+                .HasOne<Tag>(it => it.Tag)
+                .WithMany(t => t.ItemTags)
+                .HasForeignKey(it => it.TagId);
+
+        }
+    }
+}
