eb02a7ecb514f3982c091a6ec6114a8cac9b6a66
PW <przemek.wrobel@poczta.fm>
2021-01-20 11:44:07 +0100

    Dodany Mapping profile (refleksja)

--- MyRentalShop/MyRentalShopMVC.Application/Mapping/MappingProfile.cs
+++ MyRentalShop/MyRentalShopMVC.Application/Mapping/MappingProfile.cs
@@ -1,12 +1,32 @@
 ﻿using AutoMapper;
 using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
 using System.Text;
 
 namespace MyRentalShopMVC.Application.Mapping
 {
     public class MappingProfile : Profile
     {
+        public MappingProfile()
+        {
+            ApplyMappingFromAssembly(Assembly.GetExecutingAssembly());
+        }
 
+        private void ApplyMappingFromAssembly(Assembly assembly)
+        {
+            var types = assembly.GetExportedTypes()
+                 .Where(t => t.GetInterfaces().Any(i =>
+                 i.IsGenericType && i.GetGenericTypeDefinition() == typeof(IMapFrom<>)))
+                 .ToList();
+
+            foreach (var type in types)
+            {
+                var instansce = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instansce, new object[] { this });
+            }
+        }
     }
 }
