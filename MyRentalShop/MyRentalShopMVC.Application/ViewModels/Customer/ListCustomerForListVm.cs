e69a5eef46f4f86b08038c73100c14649ccc5548
PW <przemek.wrobel@poczta.fm>
2021-02-03 13:47:08 +0100

    Rozszerzenie vm Użytkowników do wyświetlenia

--- MyRentalShop/MyRentalShopMVC.Application/ViewModels/Customer/ListCustomerForListVm.cs
+++ MyRentalShop/MyRentalShopMVC.Application/ViewModels/Customer/ListCustomerForListVm.cs
@@ -6,7 +6,26 @@ namespace MyRentalShopMVC.Application.ViewModels.Customer
 {
     public class ListCustomerForListVm
     {
+        /// <summary>
+        /// Lista użytkowników do wyświetlenia
+        /// </summary>
         public List<CustomerForListVm> Customers { get; set; }
+        
+        /// <summary>
+        /// Przechowuju info o nr strony która jest wyswietlana
+        /// </summary>
+        public int CurrentPage { get; set; }
+
+        /// <summary>
+        /// Przechowuje info o ilości wierszy na stronie
+        /// </summary>
+        public int PegeSize { get; set; }
+
+        /// <summary>
+        /// Przechowuje bierzące wyszukiwanie
+        /// </summary>
+        public string SearchString { get; set; }
+
         public int Count { get; set; }
 
     }
