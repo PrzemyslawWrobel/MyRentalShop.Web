353409f193ab41c77b903ac749307531cb6ec48d
Przemyslaw Wrobel <przemek.wrobel@poczta.fm>
2021-02-15 13:00:23 +0100

    Update - metoda do zapisu edycji użytkownika

--- MyRentalShop/MyRentalShopMVC.Application/Interfaces/ICustomerService.cs
+++ MyRentalShop/MyRentalShopMVC.Application/Interfaces/ICustomerService.cs
@@ -13,5 +13,6 @@ namespace MyRentalShopMVC.Application.Interfaces
 
         //TODO Zmienić NewCustomerVm na pełny model do edycji
         NewCustomerVm GetCustomerForEdit(int id);
+        void UpdateCustomer(NewCustomerVm model);
     }
 }
