b06dd6c69263678dfe89dac9d8f2dff474a3f6b1
Przemyslaw Wrobel <przemek.wrobel@poczta.fm>
2021-02-17 12:40:18 +0100

    Identity initial

---
+++ MyRentalShop/MyRentalShop.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRentalShopMVC.Infrastructure;
+
+[assembly: HostingStartup(typeof(MyRentalShop.Web.Areas.Identity.IdentityHostingStartup))]
+namespace MyRentalShop.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
