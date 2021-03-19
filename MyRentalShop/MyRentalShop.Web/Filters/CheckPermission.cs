98d4fecf9ac356864126fe7e51f65d8cf1c7dbf4
Przemyslaw Wrobel <przemek.wrobel@poczta.fm>
2021-03-19 10:30:28 +0100

    Dodanie i implementacja interfejsu IAuthorizationFilte

--- MyRentalShop/MyRentalShop.Web/Filters/CheckPermission.cs
+++ MyRentalShop/MyRentalShop.Web/Filters/CheckPermission.cs
@@ -1,11 +1,16 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
 namespace MyRentalShop.Web.Filters
 {
-    public class CheckPermission : Attribute
+    public class CheckPermission : Attribute, IAuthorizationFilter
     {
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
