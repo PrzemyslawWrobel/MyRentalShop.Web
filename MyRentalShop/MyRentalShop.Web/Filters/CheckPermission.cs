2d364966cb0f8bcc1f12b04d89f543b433916f7c
Przemyslaw Wrobel <przemek.wrobel@poczta.fm>
2021-03-19 10:26:24 +0100

    Dodanie katalogu z własnymi filtrami autoryzacyjnymi

---
+++ MyRentalShop/MyRentalShop.Web/Filters/CheckPermission.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyRentalShop.Web.Filters
+{
+    public class CheckPermission : Attribute
+    {
+    }
+}
